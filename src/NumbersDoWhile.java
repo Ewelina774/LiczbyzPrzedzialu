2e212fe4b75088db0e51f5b3dc1ca0dd1448c28a
Ewelina774 <ewelina_hofman98@wp.pl>
2019-07-25 11:48:12 +0200

    Dodanie dwóch plików

---
+++ src/NumbersDoWhile.java
@@ -0,0 +1,8 @@
+public class NumbersDoWhile {
+    public static void main(String[] args) {
+        double i = 0;
+        do {
+            System.out.println("Liczba " + i/10); i++;
+        } while(i <= 30);
+    }
+}
